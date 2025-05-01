b691f861842b1482276e0e4bc6fc411e07df67f0
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-05-02 00:57:02 +0700

    done: usercontroller

--- src/main/java/com/individu/crud_spring_boot/controller/UserController.java
+++ src/main/java/com/individu/crud_spring_boot/controller/UserController.java
@@ -1,4 +1,39 @@
 package com.individu.crud_spring_boot.controller;
 
+import com.individu.crud_spring_boot.dto.UserRequest;
+import com.individu.crud_spring_boot.dto.UserResponse;
+import com.individu.crud_spring_boot.service.UserService;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+@NoArgsConstructor
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("create-user")
+    String createUser(@RequestBody UserRequest request){
+        return userService.createUser(request);
+    }
+
+    @GetMapping("/get-all-users")
+    List<UserResponse> getAll(@RequestParam(required = false, defaultValue = "") String name){
+        return userService.getAll(name);
+    }
+
+    @PutMapping("/edit/{id}")
+    String updateUser(@PathVariable String id, @RequestBody UserRequest request){
+        return userService.updateUser(id, request);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    String deleteUser(@PathVariable String id){
+        return userService.deleteUser(id);
+    }
 }
--- src/main/java/com/individu/crud_spring_boot/service/UserService.java
+++ src/main/java/com/individu/crud_spring_boot/service/UserService.java
@@ -1,4 +1,13 @@
 package com.individu.crud_spring_boot.service;
 
-public class UserService {
+import com.individu.crud_spring_boot.dto.UserRequest;
+import com.individu.crud_spring_boot.dto.UserResponse;
+
+import java.util.List;
+
+public interface UserService {
+    String createUser(UserRequest request);
+    List<UserResponse> getAll(String name);
+    String updateUser(String id, UserRequest request);
+    String deleteUser(String id);
 }
