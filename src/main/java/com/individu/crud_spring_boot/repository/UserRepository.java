987a4eac465597183555717783e27595b67fd0a0
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-05-07 12:31:34 +0700

    edit UserRepository & UserServiceImpl

--- src/main/java/com/individu/crud_spring_boot/repository/UserRepository.java
+++ src/main/java/com/individu/crud_spring_boot/repository/UserRepository.java
@@ -3,6 +3,11 @@ package com.individu.crud_spring_boot.repository;
 
 import com.individu.crud_spring_boot.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<Users, String> {
+    @Query("SELECT u FROM Users u WHERE u.username LIKE CONCAT('%', :username, '%')")
+    List<Users> findByUsername(String name);
 }
--- src/main/java/com/individu/crud_spring_boot/service/impl/UserServiceImpl.java
+++ src/main/java/com/individu/crud_spring_boot/service/impl/UserServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
